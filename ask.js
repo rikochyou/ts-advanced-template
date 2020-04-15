8174611d30e55d36cfb30045ed023c53b10f698f
riko <rikochyou@outlook.com>
2020-04-15 17:41:52 +0800

    init

---
+++ ask.js
@@ -0,0 +1,21 @@
+module.exports = [{
+  type: 'input',
+  name: 'name',
+  message: 'project name?'
+},
+  {
+    type: 'input',
+    name: 'version',
+    message: 'project version?'
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'author'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'description?'
+  }
+]
